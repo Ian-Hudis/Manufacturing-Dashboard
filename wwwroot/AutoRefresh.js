d49310f96b4d952843df27510a61ce8aac259101
Ian Hudis <52722993+Ian-Hudis@users.noreply.github.com>
2026-01-19 08:41:26 -0600

    Add files via upload

---
+++ wwwroot/AutoRefresh.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    window.AutoRefresh = {
+        refreshMTconnectData: () => {
+            setInterval(() => {
+                document.getElementById("TableRefreshBUT").click();
+            }, 1000);
+            setInterval(() => {
+                document.getElementById("TableRefreshMainPage").click();
+            }, 50000);
+            
+        }
+    };
+})();
